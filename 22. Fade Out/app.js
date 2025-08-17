b26f60e2090af24c9d52bec0f7d8d7e04ec7165e
Utkarsh Kamra <utkkamra@gmail.com>
2025-08-17 23:56:45 +0530

    Fade In/Out

---
+++ 22. Fade Out/app.js
@@ -0,0 +1,11 @@
+let arrow = document.querySelector(".arrow");
+
+window.addEventListener('scroll', () => {
+    let position = window.scrollY;
+    // console.log(position)
+
+    if(position <= 5){
+        arrow.classList.toggle('fade-in')
+        arrow.classList.toggle('fade-out')
+    }
+})
